44171a2ac31e48466b5b6b66d3e5eabeff7858bf
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-04-22 03:26:04 +0200

    .

--- webbrowser.js
+++ webbrowser.js
@@ -1,8 +1,16 @@
 const { Builder, Browser, By, Key, until } = require('selenium-webdriver');
 
+let driver;
+
 openGoogle;
 
 async function openGoogle() {
-    let driver = await new Builder().forBrowser(Browser.CHROME).build();
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
     await driver.get('https://www.google.com');
+    await click('Ich stimme zu');
+    await driver.findElement(By.name('q')).sendKeys('Developer Akademie', Key.RETURN)
+}
+
+async function click(text) {
+    await driver.findElement(By.xpath("//*[text()='" + text + "']")).click();
 }
